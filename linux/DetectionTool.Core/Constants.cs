1cb14e4a1daa00b422f8a4900619ef6af1ff209b
MrLoLf <roscher2003@outlook.de>
2023-06-08 19:39:54 +0200

    removed testing folder

--- linux/DetectionTool.Core/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace DetectionTool.Core {
-  public static class Constants {
-    public const string kSupportArticle = "https://support.curseforge.com/en/support/solutions/articles/9000228509-june-2023-infected-mods-detection-tool";
-
-  }
-}
