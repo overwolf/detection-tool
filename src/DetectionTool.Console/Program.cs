6b4eaa33ed4a14ba85308a54fd4ea17b5041499a
MrLoLf <roscher2003@outlook.de>
2023-06-22 14:51:19 +0200

    copied to remove styling changes

--- src/DetectionTool.Console/Program.cs
+++ src/DetectionTool.Console/Program.cs
@@ -3,34 +3,34 @@
 RunScan();
 
 static void RunScan() {
-  var scanner = new Scanner();
+    var scanner = new Scanner();
 
-  try {
-    var results = scanner.Scan();
+    try {
+        var results = scanner.Scan();
 
-    if (results.Detected) {
-      Console.ForegroundColor = ConsoleColor.Red;
-      Console.WriteLine("Detected: YES");
-      Console.WriteLine("Possible Malware traces were detected on your machine");
-      Console.WriteLine();
-      Console.WriteLine("Suspicious Files:");
-      foreach(var detectedFile in results.DetectedFiles) {
-        Console.WriteLine($"  => {detectedFile}");
-      }
-      Console.WriteLine();
-      Console.ForegroundColor = ConsoleColor.Blue;
-      Console.WriteLine($"For instructions on what to do next go to: {DetectionTool.Core.Constants.kSupportArticle}");
+        if (results.Detected) {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Detected: YES");
+            Console.WriteLine("Possible Malware traces were detected on your machine");
+            Console.WriteLine();
+            Console.WriteLine("Suspicious Files:");
+            foreach(var detectedFile in results.DetectedFiles) {
+                Console.WriteLine($"  => {detectedFile}");
+            }
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"For instructions on what to do next go to: {DetectionTool.Core.Constants.kSupportArticle}");
 
-    } else {
-      Console.ForegroundColor = ConsoleColor.Green;
-      Console.WriteLine("Detected: NO");
-      Console.WriteLine("Malware was not detected on your machine");
+        } else {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Detected: NO");
+            Console.WriteLine("Malware was not detected on your machine");
+        }
+    } catch (Exception ex) {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("Detected: Inconclusive");
+        Console.WriteLine($"Scan failed. {ex.Message}");
     }
-  } catch (Exception ex) {
-    Console.ForegroundColor = ConsoleColor.Yellow;
-    Console.WriteLine("Detected: Inconclusive");
-    Console.WriteLine($"Scan failed. {ex.Message}");
-  }
 
-  Console.ResetColor();
+    Console.ResetColor();
 }
